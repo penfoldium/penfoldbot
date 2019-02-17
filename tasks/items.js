b9cfdeb27af6c9638382f3e6f7d9c2addd9a11b5
Alex <kovacs.alex26@gmail.com>
2019-02-17 20:55:52 +0200

    quick fix: if the user is a bot or doesn't have any items added return

--- tasks/items.js
+++ tasks/items.js
@@ -13,7 +13,9 @@ module.exports = class extends Task {
         if (this.client.settings.fortniteitems === res.date) return;
         await this.client.settings.update('fortniteitems', res.date);
         this.client.users.forEach(async (u) => {
+            if (u.bot) return;
             const settings = await u.settings.get('fortniteitems');
+            if (!settings.length) return;
             const items = res.items.map(i => i.name);
             const common = items.filter(i => settings.includes(i.toLowerCase()));
             if (common.length) u.send(`One or more items you wanted to be notified about are in the Fortnite Item Shop: \`${common.join(', ')}\``);
