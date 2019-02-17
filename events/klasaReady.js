df8aa730d4c321d592e18d0be744e102b30194bc
Alex <kovacs.alex26@gmail.com>
2019-02-17 20:38:00 +0200

    add playing status

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -12,6 +12,10 @@ module.exports = class extends Event {
     }
 
     async run() {
+
+        await this.client.user.setActivity(`Danger Mouse | ${this.client.options.prefix}help`, { type: "WATCHING" });
+        await this.client.user.setStatus('dnd');
+
         const s = this.client.settings.schedules;
         if (s.some(e => e.taskName === 'items')) return;
         this.client.schedule.create("items", "*/5 * * * *");
