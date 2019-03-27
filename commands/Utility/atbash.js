84c845cc7d717ee9294c90d367a242cd5918b96c
Alex <kovacs.alex26@gmail.com>
2019-03-27 17:59:38 +0200

    change the usage for code consistency, format the code and mode the atBash function to be separate from the run function

--- commands/Utility/atbash.js
+++ commands/Utility/atbash.js
@@ -1,5 +1,5 @@
 const { Command } = require('klasa');
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Command {
 
@@ -20,26 +20,26 @@ module.exports = class extends Command {
             subcommands: false,
             description: 'Encode/decode with the Atbash cipher',
             quotedStringSupport: false,
-            usage: '<args:string> [...]',
+            usage: '<text:...string>',
             usageDelim: ' ',
             extendedHelp: 'No extended help available.'
         });
-        this.customizeResponse('args', "I can't help you if you won't tell me what to cipher, chief.")
+        this.customizeResponse('text', "I can't help you if you won't tell me what to cipher, chief.")
     }
 
-    async run(message, [...args]) {
-    function atbash(input) {
+    async run(message, [text]) {
+        const embed = new MessageEmbed()
+            .setAuthor(`Atbash Cipher`, this.client.user.displayAvatarURL({ format: 'png', size: 2048 }))
+            .setDescription(`**Original text**\n\`${text}\`\n\n**Cipher text**\n\`${this.atbash(text)}\``)
+            .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', size: 2048 }))
+            .setColor('#2e7da4')
+            .setTimestamp();
+        return message.send(embed)
+    }
+
+    atbash(input) {
         return input.replace(/[a-z]/gi,
             c => String.fromCharCode(-c.charCodeAt() + (/[a-z]/.test(c) ? 219 : 155)));
-    }
-
-    const embed = new Discord.MessageEmbed()
-    .setAuthor(`Atbash Cipher`, this.client.user.displayAvatarURL({format: 'png', size: 2048}))
-    .setDescription(`**Original text**\n\`${args.join(' ')}\`\n\n**Cipher text**\n\`${atbash(args.join(' '))}\``)
-    .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({format: 'png', size: 2048}))
-    .setColor('#2e7da4')
-    .setTimestamp();
-    message.send(embed)
     }
 
     async init() {
