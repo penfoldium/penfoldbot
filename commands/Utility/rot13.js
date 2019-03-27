f3a292ea2dbe3d8f11dcfeeb8ab17f92238e640f
Alex <kovacs.alex26@gmail.com>
2019-03-27 18:02:17 +0200

    change usage for code consistency (rot13)

--- commands/Utility/rot13.js
+++ commands/Utility/rot13.js
@@ -20,23 +20,23 @@ module.exports = class extends Command {
             subcommands: false,
             description: 'Encode/decode with the ROT13 cipher',
             quotedStringSupport: false,
-            usage: '<args:string> [...]',
+            usage: '<text:...string>',
             usageDelim: ' ',
             extendedHelp: 'No extended help available.'
         });
-        this.customizeResponse('args', "I can't help you if you won't tell me what to cipher, chief.")
+        this.customizeResponse('text', "I can't help you if you won't tell me what to cipher, chief.")
     }
 
-    async run(message, [...args]) {
-    let toCipher = args.join(' ');
-    let ciphered = toCipher.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
-    const embed = new Discord.MessageEmbed()
-    .setAuthor(`ROT13 Cipher`, this.client.user.displayAvatarURL({format: 'png', size: 2048}))
-    .setDescription(`**Original text**\n\`${toCipher}\`\n\n**Cipher text**\n\`${ciphered}\``)
-    .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({format: 'png', size: 2048}))
-    .setColor('#2e7da4')
-    .setTimestamp();
-    message.send(embed)
+    async run(message, [text]) {
+        let toCipher = text;
+        let ciphered = toCipher.replace(/[a-zA-Z]/g, function (c) { return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26); });
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(`ROT13 Cipher`, this.client.user.displayAvatarURL({ format: 'png', size: 2048 }))
+            .setDescription(`**Original text**\n\`${toCipher}\`\n\n**Cipher text**\n\`${ciphered}\``)
+            .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', size: 2048 }))
+            .setColor('#2e7da4')
+            .setTimestamp();
+        return message.send(embed)
     }
 
     async init() {
