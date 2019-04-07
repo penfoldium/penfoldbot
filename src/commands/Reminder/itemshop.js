61aa13dbcb5ba75c830cd1131964461595cfa0d7
Alex <kovacs.alex26@gmail.com>
2019-04-07 10:35:48 +0300

    add the ability to view what items are available in the daily itemshop (itemshop shop | daily)

--- src/commands/Reminder/itemshop.js
+++ src/commands/Reminder/itemshop.js
@@ -19,18 +19,22 @@ module.exports = class extends Command {
             subcommands: false,
             description: 'Set reminders that go off when specified items become available in Fortnite\'s item shop! Spend your V-Bucks wisely!',
             quotedStringSupport: false,
-            usage: '[list] [item:...string]',
+            usage: '[list] [shop|daily] [item:...string]',
             usageDelim: ' ',
             extendedHelp: 'No extended help available.'
         });
     }
 
-    async run(message, [list, item]) {
+    async run(message, [list, shop, item]) {
+
+        if (shop) {
+            if (!this.client._itemshop) throw "Something went wrong, try again later.";
+            return message.send(`Here are all the currently available items: ${this.client._itemshop.map(item => item.name).join(', ')}`);
+        }
 
         if (list) {
             const settings = await message.author.settings.get('fortniteitems');
-            message.send(settings.length < 1 ? "You don\'t have any items on your list, chief!" : `Here are the items on your reminder list: \`${settings.join(', ')}\``)
-            return;
+            return message.send(settings.length < 1 ? "You don\'t have any items on your list, chief!" : `Here are the items on your reminder list: \`${settings.join(', ')}\``);
         }
 
         if (!item) throw 'Tell me the item you\'re interested in and I\'ll make sure to remind you about it when it\'s available!';
