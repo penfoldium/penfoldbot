5b2e9dc45560cd7b3044cacdf0168953895ac5df
YoshiFan (Seb) <kyorochan2000@gmail.com>
2019-04-09 00:22:10 +0300

    Remove space that could cause confusion

--- src/commands/General/info.js
+++ src/commands/General/info.js
@@ -19,7 +19,7 @@ module.exports = class extends Command {
 ❯ Hello, chief! I am **Penfold**, Danger Mouse's loyal sidekick, and your faithful personal assistant on Discord! Nice to meet you! :hamster: :heart:
 ❯ I can help you with reminders and various utilities. To get started, send \`penfold, help\` to see what I can do! (I am mentionable as well, so you can use \`@Penfold help\`)
 ❯ *Prefixes are case insensitive, so you could also yell at me, but I don't like it when you do that* :cry:
-❯ (To change the prefix, run \`<currentPrefix> prefix <newPrefix>\`; to reset it, run \`<currentPrefix> prefix reset\`)
+❯ (To change the prefix, run \`<currentPrefix>prefix <newPrefix>\`; to reset it, run \`<currentPrefix>prefix reset\`)
 ❯ If you want to see how my code works, you can access it [here](https://github.com/penfoldium/penfoldbot)!
 ❯ My creators are \`YoshiFan13#4985\` and \`alex ☿ raplayer#0001\` - if you find any issues or have any ideas for new features, don't hesitate to contact them!
 ❯ Alternatively, you can submit an issue on the Git linked above.
