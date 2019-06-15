290b55b443d6772a9222ce7886e3b8d0837a5911
YoshiFan (Seb) <kyorochan2000@gmail.com>
2019-06-15 23:09:13 +0300

    the other commands xd

---
+++ src/commands/Fun/8ball.js
@@ -0,0 +1,51 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            name: '8ball',
+            enabled: true,
+            runIn: ['text', 'dm'],
+            cooldown: 5,
+            deletable: true,
+            bucket: 1,
+            aliases: ['ask', 'question'],
+            guarded: false,
+            nsfw: false,
+            permissionLevel: 0,
+            requiredPermissions: [],
+            requiredSettings: [],
+            subcommands: false,
+            description: "Ask the Magic 8-Ball for an answer...",
+            quotedStringSupport: false,
+            usage: '<question:reg/\\w+\\?$/>',
+            usageDelim: undefined,
+            extendedHelp: 'No extended help available.'
+        });
+
+        this.customizeResponse('question', `You're supposed to ask it a question, chief!\n(End with a question mark!)`)
+    }
+
+    async run(message) {
+        let ball = await fetch(`https://nekos.life/api/v2/8ball`);
+        ball = await ball.json();
+
+        const embed = new MessageEmbed()
+            .setColor(this.client.options.config.embedHex)
+            .setDescription(`The Magic 8-Ball says... ${ball.response}`)
+            .setImage(ball.url)
+            .setAuthor(`Magic 8-Ball`, this.client.user.displayAvatarURL({ format: 'png', size: 2048 }))
+            .setTimestamp()
+            .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.displayAvatarURL({ format: 'png', size: 2048 }));
+
+        return message.send(embed);
+    }
+
+    async init() {
+
+    }
+
+};
---
+++ src/commands/Fun/joke.js
@@ -0,0 +1,48 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			name: 'joke',
+            enabled: true,
+            runIn: ['text', 'dm'],
+            cooldown: 5,
+            deletable: true,
+            bucket: 1,
+			aliases: [],
+			guarded: false,
+            nsfw: false,
+            permissionLevel: 0,
+            requiredPermissions: ["EMBED_LINKS"],
+            requiredSettings: [],
+            subcommands: false,
+			description: 'Get a random joke',
+			quotedStringSupport: false,
+			usage: '',
+			usageDelim: '',
+			extendedHelp: 'No extended help available.'
+		});
+	}
+
+	async run(message) {
+		let joke = await fetch(`https://official-joke-api.appspot.com/jokes/random`);
+		joke = await joke.json();
+
+		const embed = new MessageEmbed()
+			.setColor(this.client.options.config.embedHex)
+			.setDescription(`${joke.setup}\n\n||${joke.punchline}||\n*Click/tap above to reveal answer*`)
+			.setAuthor(`Random Joke`, this.client.user.displayAvatarURL({ format: 'png', size: 2048 }))
+			.setTimestamp()
+			.setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ format: 'png', size: 2048 }));
+
+		return message.send(embed);
+	}
+
+	async init() {
+
+	}
+
+};
