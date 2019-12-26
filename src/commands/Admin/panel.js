df49f290592dea962e8d6ae4c3579bce41e10cf8
YoshiFan (Seb) <kyorochan2000@gmail.com>
2019-12-26 21:56:19 +0200

    refinementsss

--- src/commands/Admin/panel.js
+++ src/commands/Admin/panel.js
@@ -1,4 +1,4 @@
-const { Command, RichDisplay } = require('klasa');
+const { Command, RichDisplay, Timestamp } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 const RichMenu = require('../../util/RichMenu');
 
@@ -12,14 +12,13 @@ module.exports = class extends Command {
             cooldown: 0,
             deletable: true,
             bucket: 1,
-            aliases: [],
+            aliases: ['adminpanel'],
             guarded: false,
             nsfw: false,
             permissionLevel: 10,
             requiredPermissions: ["EMBED_LINKS"],
             requiredSettings: [],
             subcommands: false,
-            // TODO: Add a description
             description: '',
             quotedStringSupport: false,
             usage: '',
@@ -50,43 +49,47 @@ module.exports = class extends Command {
             const guilds = this.client.guilds.map(async g => {
                 const members = await g.members.fetch();
                 const bots = members.filter(m => m.user.bot).size;
+                const textChannels = g.channels.filter(c => c.type === 'text').size;
+                const voiceChannels = g.channels.filter(c => c.type === 'voice').size;
+                const categoryChannels = g.channels.size - textChannels - voiceChannels;
                 return `Name: ${g.name}
-ID: ${g.id}
+ID: \`${g.id}\`
 Owner: ${g.owner}
-Members: ${g.memberCount} (of which ${Math.floor((bots / g.memberCount) * 100)}% bots)
-Created: ${g.createdAt}`
+Channels: ${g.channels.size} (${textChannels} text and ${voiceChannels} voice in ${categoryChannels} categories)
+Members: ${g.memberCount} (of which ${bots} bots)
+Created: ${new Timestamp('LL').display(g.createdAt)}`
             });
 
             const guildsResolved = await Promise.all(guilds);
             guildsResolved.forEach(guildInfo => display.addPage(embed => embed.setTitle('Guild Info').setDescription(guildInfo)));
-            return display.run(collector.message);
+            return display.run(collector.message, { filter: (reaction, user) => user === message.author });
         }
 
         // Reinitialize status
         else if (choice == 1) {
             this.client.emit('klasaReady');
             await collector.message.delete();
-            return message.send(':ok_hand:');
+            return message.send(':white_check_mark: Done!');
         }
 
         // Set status
         else if (choice == 2) {
-            const collector2 = await this.statusMenu.run(collector.message);
+            const collector2 = await this.statusMenu.run(collector.message, { filter: (reaction, user) => user === message.author });
             const choice2 = await collector2.selection;
             const status = choice2 == 0 ? 'online' : choice2 == 1 ? 'idle' : choice2 == 2 ? 'invisible' : choice2 == 3 ? 'dnd' : 'online';
 
             if (status === 'invisible') {
                 this.client.user.setStatus(status);
-                return message.send(':ok_hand:');
+                return message.send(':white_check_mark: Done!');
             } else {
                 const [response] = await this.askText.createPrompt(message, { target: message.author }).run('Do you want to edit the current presence text?');
                 if (response) {
                     const [text] = await this.askPresenceText.createPrompt(message, { target: message.author }).run('Please write the presence text you want to use!');
                     this.client.user.setPresence({ activity: { name: text }, status })
-                    return message.send(':ok_hand:');
+                    return message.send(':white_check_mark: Done!');
                 } else {
                     this.client.user.setStatus(status);
-                    return message.send(':ok_hand:');
+                    return message.send(':white_check_mark: Done!');
                 }
             }
         }
@@ -94,16 +97,16 @@ Created: ${g.createdAt}`
         // Reboot
         else if (choice == 3) {
             await collector.message.delete();
-            await message.send('See you on the other world, chief!');
+            await message.send('Just a quick little nap, chief... :hamster::zzz:');
             process.exit();
         }
 
         // Leave servers
         else if (choice == 4) {
-            const [guild] = await this.askGuildID.createPrompt(message).run('What guild would you like the bot to leave?');
+            const [guild] = await this.askGuildID.createPrompt(message).run('What server would you like me to leave?');
             if (guild) {
                 await this.client.guilds.get(guild.id).leave();
-                return message.send(guild)
+                return message.send(`:white_check_mark: Left \`${guild.name}\` successfully.`)
             }
         }
     }
@@ -124,7 +127,7 @@ Created: ${g.createdAt}`
         this.statusMenu.addOption("Online", "Set the status to online");
         this.statusMenu.addOption("Idle", "Set the status to idle");
         this.statusMenu.addOption("Invisible", "Set the status to invisible");
-        this.statusMenu.addOption("DND", "Set your status to DND");
+        this.statusMenu.addOption("DND", "Set the status to DND");
     }
 
 };
