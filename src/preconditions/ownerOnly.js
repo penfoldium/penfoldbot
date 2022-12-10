792d82064c837688d0816246a05636adf6833361
Alex Kovacs <alexdemaster@proton.me>
2022-12-10 09:53:50 +0200

    add ownerOnly precondition

--- src/preconditions/ownerOnly.js
+++ src/preconditions/ownerOnly.js
@@ -2,24 +2,18 @@ const { Precondition } = require("@sapphire/framework");
 
 class OwnerOnlyPrecondition extends Precondition {
   /**
-   * @param {import('discord.js').Message} message
-   */
-  messageRun(message) {
-    return this.ok();
-  }
-
-  /**
    * @param {import('discord.js').CommandInteraction} interaction
    */
-  chatInputRun(interaction) {
-    return this.ok();
-  }
-
-  /**
-   * @param {import('discord.js').ContextMenuInteraction} interaction
-   */
-  contextMenuRun(interaction) {
-    return this.ok();
+  async chatInputRun(interaction) {
+    await interaction.client.application.fetch();
+    const owner = interaction.client.application.owner.members
+      ? Array.from(interaction.client.application.owner.members.values()).map(
+          (member) => member.user.tag
+        )
+      : [interaction.client.application.owner.tag];
+    if (owner.includes(interaction.user.tag)) return this.ok();
+    else
+      return this.error({ message: "Only bot owners can use this command." });
   }
 }
 
