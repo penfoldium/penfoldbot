da962a073b958b8bb32ac7f277fc2365a2ce7f1e
Alex Kovacs <alex@kvcs.dev>
2025-12-30 15:56:25 +0200

    feat(commands): add exec

---
+++ src/Commands/Admin/Exec.ts
@@ -0,0 +1,62 @@
+import {
+  codeBlock,
+  SlashCommandNumberOption,
+  SlashCommandStringOption,
+  type ChatInputCommandInteraction,
+} from "discord.js";
+import ms from "ms";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+import type { PenfoldClient } from "../../Classes/PenfoldClient.js";
+import { PenfoldCommand } from "../../Classes/PenfoldCommand.js";
+
+const execPromise = promisify(exec);
+
+export default class extends PenfoldCommand {
+  constructor(client: PenfoldClient) {
+    super(client, {
+      name: "exec",
+      description: "Execute JS",
+      ownerOnly: true,
+    });
+
+    this.builder
+      .addStringOption(
+        new SlashCommandStringOption()
+          .setName("str")
+          .setDescription("String to execute")
+          .setRequired(true)
+      )
+      .addNumberOption(
+        new SlashCommandNumberOption()
+          .setName("timeout")
+          .setDescription("The amount of time in ms for the command to timeout")
+          .setRequired(false)
+      );
+  }
+
+  public async run(interaction: ChatInputCommandInteraction) {
+    await interaction.reply("Executing your command...");
+    const input = interaction.options.getString("str", true);
+    const timeout = interaction.options.getNumber("timeout");
+
+    const start = performance.now();
+    const result = await execPromise(input, {
+      timeout: timeout ?? 60000,
+    }).catch((error) => ({ stdout: null, stderr: error }));
+    const output = result.stdout
+      ? `**\`OUTPUT\`**${codeBlock("prolog", result.stdout)}`
+      : "";
+    const outerr = result.stderr
+      ? `**\`ERROR\`**${codeBlock("prolog", result.stderr)}`
+      : "";
+    const end = performance.now();
+
+    await interaction.editReply({
+      content:
+        ([output, outerr].join("\n") ||
+          "Done. There was no output to stdout or stderr.") +
+        `\nTook: ${ms(Number((end - start).toFixed(2)))} to execute ⌛`,
+    });
+  }
+}
