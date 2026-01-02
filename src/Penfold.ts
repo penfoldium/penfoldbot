c01a5f39e6180720ed73da6c71bebc613d82bf36
Alex Kovacs <alex@kvcs.dev>
2026-01-02 17:13:58 +0200

    feat: add dailydm functionality

--- src/Penfold.ts
+++ src/Penfold.ts
@@ -3,7 +3,11 @@ import { PenfoldClient } from "./Classes/PenfoldClient.js";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
+import timezone from "dayjs/plugin/timezone.js";
+import utc from "dayjs/plugin/utc.js";
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 if (!process.env["DATABASE_URL"]) throw new Error("Please populate the DATABASE_URL env variable.");
 
---
+++ src/Tasks/DailyDM.ts
@@ -0,0 +1,93 @@
+import dayjs from "dayjs";
+import { EmbedBuilder } from "discord.js";
+import ms from "ms";
+import type { PenfoldClient } from "../Classes/PenfoldClient.js";
+import { PenfoldTask } from "../Classes/PenfoldTask.js";
+
+export default class extends PenfoldTask {
+  constructor(client: PenfoldClient) {
+    super(client, {
+      name: "DailyDM",
+      cron: "* * * * *",
+      clientReady: true
+    });
+  }
+
+  public async run() {
+    const dailydm = await this.client.db.userSettings.findMany({
+      where: {
+        dailyDmEnabled: true
+      }
+    });
+
+    function filter(daily: {
+      id: bigint;
+      timezone: string;
+      dailyDmTime: string;
+      dailyDmEnabled: boolean;
+    }) {
+      const [hour, minute] = daily.dailyDmTime.split(":");
+      const day = dayjs()
+        .tz(daily.timezone)
+        .set("hours", Number(hour))
+        .set("minutes", Number(minute))
+        .set("seconds", 0);
+      return day.diff() <= 10 && day.diff() >= -10;
+    }
+
+    const toFire = dailydm.filter(filter);
+    if (!toFire) return;
+
+    for (const daily of toFire) {
+      await this.sendDaily(daily);
+    }
+  }
+
+  async sendDaily(daily: {
+    id: bigint;
+    timezone: string;
+    dailyDmTime: string;
+    dailyDmEnabled: boolean;
+  }) {
+    const user = await this.client.users
+      .fetch(daily.id.toString())
+      .catch(err => this.client.debug(`Something went wrong when fetching user (dailydm): ${err}`));
+    if (!user) return;
+
+    const reminders = await this.client.db.reminders
+      .findMany({
+        where: {
+          user_id: daily.id,
+          triggered: false
+        }
+      })
+      .catch(() => {});
+
+    if (!reminders) return;
+
+    const filteredReminders = reminders.filter(reminder => {
+      return dayjs().diff(reminder.date, "days") == 0;
+    });
+
+    if (!filteredReminders.length) return;
+
+    const fields = [];
+
+    for (const reminder of filteredReminders) {
+      fields.push({
+        name: `Reminder \`${reminder.id}\` (due in ${ms(Math.abs(dayjs().tz(daily.timezone).diff(reminder.date)), { long: true })})`,
+        value: `\`${reminder.message}\``
+      });
+    }
+
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: `Daily DM for ${user.username}`, iconURL: user.displayAvatarURL() })
+      .setFields(fields);
+
+    let dm = await user.dmChannel?.fetch();
+    if (!dm) dm = await user.createDM();
+    await dm.send({
+      embeds: [embed]
+    });
+  }
+}
