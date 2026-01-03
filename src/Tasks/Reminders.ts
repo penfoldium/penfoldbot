ffab35c4f11945442c343fb5b34d3e363ea6f94b
Alex Kovacs <alex@kvcs.dev>
2026-01-03 23:25:03 +0200

    feat(reminders): implement a collection based queue instead of writing to db before sending

--- src/Tasks/Reminders.ts
+++ src/Tasks/Reminders.ts
@@ -1,9 +1,11 @@
 import dayjs from "dayjs";
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection } from "discord.js";
 import type { PenfoldClient } from "../Classes/PenfoldClient.js";
 import { PenfoldTask } from "../Classes/PenfoldTask.js";
 
 export default class extends PenfoldTask {
+  toSend: Collection<number, boolean> = new Collection();
+
   constructor(client: PenfoldClient) {
     super(client, {
       name: "Reminders",
@@ -19,27 +21,34 @@ export default class extends PenfoldTask {
       }
     });
 
-    const remindersToFire = reminders.filter(reminder => dayjs(reminder.date).diff() < 1);
+    const remindersToFire = reminders.filter(
+      reminder => dayjs(reminder.date).diff() < 1 && !this.toSend.has(reminder.id)
+    );
 
-    // If there are reminders with less than 1 minute remaining, mark them as triggered and send them using a setTimeout
+    // If there are reminders with less than 1 minute remaining, add them to a Collection and send them using a setTimeout
     const remindersOneMinute = reminders.filter(
       reminder =>
-        dayjs(reminder.date).diff(dayjs(), "minute") < 1 && !remindersToFire.includes(reminder)
+        dayjs(reminder.date).diff(dayjs(), "minute") < 1 &&
+        !remindersToFire.includes(reminder) &&
+        !this.toSend.has(reminder.id)
     );
 
     for (const reminder of remindersOneMinute) {
-      await this.client.db.reminders.update({
-        where: {
-          id: reminder.id
-        },
-        data: {
-          triggered: true
-        }
-      });
+      this.toSend.set(reminder.id, true);
 
       setTimeout(
         async () => {
           await this.sendReminder(reminder);
+          await this.client.db.reminders.update({
+            where: {
+              id: reminder.id
+            },
+            data: {
+              triggered: true
+            }
+          });
+
+          this.toSend.delete(reminder.id);
         },
         // We use Math.abs() here to convert negative number to positive
         Math.abs(dayjs().diff(reminder.date))
